6563834ce488f75d61096b425a7b58ae5debfd75
Pasecinic Catalina <stirbu1998@gmail.com>
2022-07-21 21:33:06 +0300

    tema pe acasa

---
+++ Text.java
@@ -0,0 +1,13 @@
+public class Text {
+    public static void main(String[] args) {
+        int x = 12;
+        if(x > 10 && x%2==0){
+            System.out.println("Numarul este par");
+        }
+        if(x<15 && x%2==1){
+            System.out.println("Numarul este impar");
+
+        }
+
+    }
+}
